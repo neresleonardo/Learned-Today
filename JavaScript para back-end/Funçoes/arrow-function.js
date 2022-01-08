6411f67f131fb82dc526ba4066d6b519d372f398
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-01-08 15:28:05 -0300

    metados

--- JavaScript para back-end/Funçoes/arrow-function.js
+++ JavaScript para back-end/Funçoes/arrow-function.js
@@ -3,3 +3,15 @@ function apresentar(nome) {
 }
 
 const apresentarArroy = name => `meu nome é ${nome}`;
+
+/*
+[04:23] Leonardo: Essa arrow function veio junto com o ES6, que foi o
+ de 2015. Foi uma grande novidade, como a Ju disse, e ela deixou nosso código bem melhor. 
+Conseguimos, agora, declarar tudo numa linha só, não precisa de chaves,
+ não precisa de return, não precisa de nada disso.
+
+[04:42] Leonardo: Ela, também, não pode ser nomeada. 
+Ela não tem nem essa possibilidade para podemos usar, então, fiquem
+ um pouco atentos com isso. E ela sempre vem com uma const e o nome da
+  nossa variável, que vamos usar antes.
+*/
---
+++ JavaScript para back-end/Introdução a arrays/aula02/index.js
@@ -0,0 +1,9 @@
+const notas = [10, 6, 8] //7
+notas.push(7)
+console.log(notas);
+
+// O elemento push ele add um dado dentro da minha const notas
+
+let media = (notas[0]+notas[1]+notas[2]+notas[3])/notas.length
+
+console.log("A media dos alunos é de : ",media);
