eacf70da56cc6bdf2c0e4e1acae1eae7ddd47280
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-01-08 13:47:13 -0300

    operadores e funçoes

---
+++ JavaScript para back-end/Funçoes/arrow-function.js
@@ -0,0 +1,5 @@
+function apresentar(nome) {
+    return `Meu nome é ${nome}` 
+}
+
+const apresentarArroy = name => `meu nome é ${nome}`;
---
+++ JavaScript para back-end/Funçoes/expressoes.js
@@ -0,0 +1,21 @@
+// Declaração de função
+
+function name(params) {
+    // bloco de codigo
+}
+
+
+// expressão de função
+
+const soma =  function (num1, num2) {
+    return num1 + num2
+}
+
+console.log(soma(1,1));
+
+/*
+A nossa expressão de função nós não temos um nome da função, 
+realmente. Nós criamos uma variável, que é o caso que criamos com const,
+e atribuiu a nossa função a essa variável.
+
+*/
