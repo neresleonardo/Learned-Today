287f95fe8d415abf8c449d5a6b9deeeda27fdd7b
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-01-08 13:20:48 -0300

    paramentros de função e funções

---
+++ JavaScript para back-end/Funçoes/Funçoes.js
@@ -0,0 +1,11 @@
+let x = "";
+
+console.log(x);
+
+x = "oi"
+
+function imprimirTexto(texto) {
+    console.log(texto);
+}
+
+imprimirTexto("Ola")
---
+++ JavaScript para back-end/Funçoes/Parâmetros_e_argumentos.js
@@ -0,0 +1,7 @@
+// Paramentro de função
+
+function soma(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(soma(3,5))
